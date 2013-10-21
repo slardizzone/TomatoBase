e40963aab38e15be17e839e3347276d841adcf8c
slardizzone <slardizzone@gmail.com>
2013-10-21 15:37:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tomatobase::Application.config.secret_key_base = 'd4cd9ad0cecff8eeb591127c76616382a4c3cd70bab2db1156591b6fd6c7eeed70693ec40aa2756c645fdc6b0571644093fca2502fbdba6f35f8b39b29ef324e'
