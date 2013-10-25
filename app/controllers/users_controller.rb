fbd75f48b94173be6aaeb978337917a20414dd42
slardizzone <slardizzone@gmail.com>
2013-10-25 11:34:41 -0400

    edited user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to search_index_path
+      redirect_to new_session_path
     else
       render :new
     end
